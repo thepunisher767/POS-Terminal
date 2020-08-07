02b99e1086bc405b15e54411b79c91c60973a698
zscholts <zachscholts@quickenloans.com>
2020-08-07 17:10:51 -0400

    Finished up Main Program. Submitting for now, but could definitely use some trimming/refactoring

--- POS Terminal/Cart.cs
+++ POS Terminal/Cart.cs
@@ -7,17 +7,6 @@ namespace POS_Terminal
     public class Cart
     {
         public static Dictionary<Product, int> shoppingCart = new Dictionary<Product, int>();
-        //public static List<Product> shoppingCart = new List<Product>();
-
-        /*public static void AddToCart(Product item, int itemCount)
-        {
-            if (shoppingCart.ContainsKey(item))
-            {
-                shoppingCart[item] += itemCount;
-            }
-            shoppingCart[item] = itemCount;
-        }
-        */
 
         public static Dictionary<Product, int> AddToCart(Product item, int quantity)
         {
@@ -28,7 +17,6 @@ namespace POS_Terminal
             shoppingCart[item] = quantity;
             return shoppingCart;
         }
-        
 
         public static double GetQuantityCost(double itemPrice, int itemCount)
         {
@@ -41,32 +29,27 @@ namespace POS_Terminal
             double subTotal = 0;
             foreach (KeyValuePair<Product, int> item in shoppingCart)
             {
-                subTotal = +item.Key.Price * item.Value;
+                subTotal += item.Key.Price * item.Value;
             }
             return subTotal;
         }
+
         public static double GetSalesTax(double subTotal)
         {
             double salesTax = subTotal * .06;
             return salesTax;
         }
+
         public static double GrandTotal(double subTotal, double salesTax)
         {
             double grandTotal = subTotal + salesTax;
             return grandTotal;
         }
+
         public static double ReturnChange(double moneyIn, double grandTotal)
         {
-            double changeToReturn = moneyIn - grandTotal;
+            double changeToReturn = Math.Round(moneyIn - grandTotal, 2);
             return changeToReturn;
         }
-        public static string TotalItemCount(Dictionary<Product, int> cart)
-        {
-            return "";
-        }
-        
-
-
-
     }
 }
--- POS Terminal/Program.cs
+++ POS Terminal/Program.cs
@@ -19,23 +19,86 @@ namespace POS_Terminal
             foodMenu.Add(new Product("Pepsi", CategoryList.Drink, "Soda", 1.99));
             foodMenu.Add(new Product("Root Beer", CategoryList.Drink, "Soda", 1.99));
             foodMenu.Add(new Product("Mountain Dew", CategoryList.Drink, "Soda", 1.99));
-            foodMenu.Add(new Product("Water", CategoryList.Drink, "Healthy drink", 1.99));
+            foodMenu.Add(new Product("Water", CategoryList.Drink, "Healthy drink", .99));
 
-            Console.WriteLine("Welcome to Paddy's Pub!");
+            Console.WriteLine("Welcome to Paddy's Pub!\n\n");
 
-            ListMenu(foodMenu);
-            bool continueFlag = true;
-            while(continueFlag)
+            bool newOrder = true;
+            while(newOrder)
             {
-                Console.Write("What would you like to order?: ");
-                Console.Write("How many would you like?: ");
-                Console.Write("Would you like to order more?: ");
-                Console.Write($"Here is the total (TOTAL). What is the amount tendered?: ");
-                Console.Write($"Here is your change: ");
-                Console.Write($"Here is your receipt: ");
+                Cart.shoppingCart.Clear();
+                ListMenu(foodMenu);
+                int userChoice = CheckMenu(foodMenu);
+                if (userChoice == 13)
+                {
+                    break;
+                }
+                string userContinue = TakeQuantity(foodMenu, userChoice);
+                while (userContinue == "y")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Would you like to see the menu again? (y/n): ");
+                    string userMenuChoice = YesOrNo(Console.ReadLine());
+                    if (userMenuChoice == "y")
+                    {
+                        Console.Clear();
+                        ListMenu(foodMenu);
+                    }
+                    userChoice = CheckMenu(foodMenu);
+                    if (userChoice == 13)
+                    {
+                        break;
+                    }
+                    userContinue = TakeQuantity(foodMenu, userChoice);
+                }
+                double subTotal = Cart.GetCartSubTotal(Cart.shoppingCart);
+                double salesTax = Cart.GetSalesTax(subTotal);
+                double grandTotal = Cart.GrandTotal(subTotal, salesTax);
+                Console.WriteLine();
+                Console.WriteLine("{0,-6} {1,-6} {2, -6}", $"Cart Subtotal: {subTotal:C2}", $"Sales Tax: {salesTax:C2}", $"Total owed: {grandTotal:C2}");
+                Console.Write($"\nWhat is the amount tendered?: ");
+                double userPayment = CashValidation(Console.ReadLine(), grandTotal);
+                double change = Cart.ReturnChange(userPayment, grandTotal);
+                Console.Write($"\nHere is your change: ${change}");
+                Console.Write($"\n\nWould you like a receipt? (y/n): ");
+                string receipt = YesOrNo(Console.ReadLine());
+                if (receipt == "y")
+                {
+                    Console.Clear();
+                    Receipt(Cart.shoppingCart, subTotal, salesTax, grandTotal, userPayment, change);
+                }
+                Console.Write($"\n\nWould you like to make another order? (y/n): ");
+                string orderAnother = YesOrNo(Console.ReadLine());
+                if (orderAnother == "y")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    newOrder = false;
+                }
             }
-
+            Console.WriteLine("\n\nThank you!!!!");
         }
+
+        public static int CheckMenu(List<Product> foodMenu)
+        {
+            Console.Write("\n\nWhat would you like to order? (menu number): ");
+            int userChoice = MenuValidation(Console.ReadLine());
+            return userChoice;
+        }
+
+        public static string TakeQuantity(List<Product> foodMenu, int userChoice)
+        {
+            Console.Write("\nHow many would you like?: ");
+            int userQuantity = QuantityValidation(Console.ReadLine());
+            Cart.AddToCart(foodMenu[userChoice - 1], userQuantity);
+            Console.WriteLine($"Added x{userQuantity} {foodMenu[userChoice - 1].Name}(s) for a total of ${Math.Round(Cart.GetQuantityCost(foodMenu[userChoice - 1].Price, userQuantity), 2)}.");
+            Console.Write("\nWould you like to order more?: ");
+            string userContinue = YesOrNo(Console.ReadLine());
+            return userContinue;
+        }
+
         public static void ListMenu(List<Product> foodMenu)
         {
             for (int i = 0; i < foodMenu.Count; i++)
@@ -43,6 +106,78 @@ namespace POS_Terminal
                 string menuOutput = String.Format("{0,-4} {1, -1}", $"{i + 1}.", $"{foodMenu[i]}");
                 Console.WriteLine(menuOutput);
             }
+            Console.WriteLine("{0,-4} {1,-1}", $"{foodMenu.Count + 1}.", "Exit");
+        }
+
+        public static double CashValidation(string userInput, double grandTotal)
+        {
+            double validDbl;
+            while (!double.TryParse(userInput, out validDbl) || validDbl < grandTotal)
+            {
+                if (!double.TryParse(userInput, out validDbl))
+                {
+                    Console.Write("Please enter a valid quantity: ");
+                    userInput = Console.ReadLine();
+                }
+                else
+                {
+                    Console.Write("Please enter an amount that will satisfy the bill: ");
+                    userInput = Console.ReadLine();
+                }
+            }
+            return Math.Round(validDbl, 2);
+        }
+
+        public static int QuantityValidation(string userInput)
+        {
+            int validInt;
+            while(!int.TryParse(userInput, out validInt))
+            {
+                Console.Write("Please enter a valid quantity: ");
+                userInput = Console.ReadLine();
+            }
+            return validInt;
+        }
+
+        public static int MenuValidation(string userInput)
+        {
+            int validInt;
+            while (!int.TryParse(userInput, out validInt) || validInt > 14 || validInt < 1)
+            {
+                Console.Write("Please enter a valid selection from the menu: ");
+                userInput = Console.ReadLine();
+            }
+            return validInt;
+        }
+
+        public static string YesOrNo(string userInput)
+        {
+            while(userInput.ToLower() != "y" && userInput.ToLower() != "n")
+            {
+                Console.Write("Please enter a valid choice (y/n): ");
+                userInput = Console.ReadLine();
+            }
+            return userInput;
+        }
+
+        public static void Receipt(Dictionary<Product,int> shoppingCart, double subTotal, double salesTax, double grandTotal, double userPayment, double change)
+        {
+            Console.WriteLine("##################################################");
+            Console.WriteLine("{0,-48} {1, 1}", "#", "#");
+            foreach (KeyValuePair<Product, int> item in shoppingCart)
+            {
+                //Console.WriteLine($"{item.Value}x {item.Key.Name}");
+                Console.WriteLine("{0,-30} {1, 6} {2,12}", $"# {item.Key.Name}", $"x{item.Value}", $"{item.Value*item.Key.Price:C2} #");
+            }
+            Console.WriteLine("{0,-48} {1, 1}", "#", "#");
+            Console.WriteLine("{0,-23} {1, 24} {2, 1}", "#", $"Subtotal: {subTotal:C2}", "#");
+            Console.WriteLine("{0,-23} {1, 24} {2, 1}", "#", $"Tax: {salesTax:C2}", "#");
+            Console.WriteLine("{0,-23} {1, 24} {2, 1}", "#", $"Total: {grandTotal:C2}", "#");
+            Console.WriteLine("{0,-23} {1, 24} {2, 1}", "#", $"=================", "#");
+            Console.WriteLine("{0,-23} {1, 24} {2, 1}", "#", $"Paid: {userPayment:C2}", "#");
+            Console.WriteLine("{0,-48} {1, 1}", "#", "#");
+            Console.WriteLine("{0,-23} {1, 24} {2, 1}", "#", $"Change: {change:C2}", "#");
+            Console.WriteLine("##################################################");
         }
     }
 }
